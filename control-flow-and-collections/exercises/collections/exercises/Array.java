a5f8b6e03cf4c0520e3dd1a86357ab5d606da63a
Alec <alecjost7@gmail.com>
2024-09-25 17:31:02 -0500

    Control flow and collections array exercise

---
+++ control-flow-and-collections/exercises/collections/exercises/Array.java
@@ -0,0 +1,10 @@
+package collections.exercises;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        for (int i = 0; i < 5; i++) {
+            System.out.println(integerArray[i]);
+        }
+    }
+}
