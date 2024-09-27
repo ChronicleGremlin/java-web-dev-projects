3b8c6c2c67191746ef374585a880b977d4e3763f
Alec <alecjost7@gmail.com>
2024-09-26 20:24:01 -0500

    part of counting characters studio

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/Main.java
@@ -0,0 +1,28 @@
+package org.launchcode;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //String to sort
+        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. " +
+                "So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. " +
+                "Once you’ve done that, it’s pretty straightforward from there.";
+
+        //Initialize HashMap
+        HashMap<Character, String> sortedCharacters = new HashMap<>();
+
+        char[] charactersInString = hiddenFigures.toCharArray();
+        do {
+            for (Character character : charactersInString) {
+                if (character == 'a'){
+
+                }
+            }
+        } while ();
+
+    }
+
+}
