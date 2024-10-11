883888be5e0717fc6eb669fa177f4eaf56e4a784
Alec <alecjost7@gmail.com>
2024-10-10 20:23:31 -0500

    main menu

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,27 @@
 package org.launchcode;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         // write your code here
+        Scanner in = new Scanner(System.in);
+
+        HashMap<String, String> menuOptions = new HashMap<>();
+        menuOptions.put("appetizer", "Appetizer");
+        menuOptions.put("breakfast", "Breakfast");
+        menuOptions.put("lunch", "Lunch");
+        menuOptions.put("dinner", "Dinner");
+        menuOptions.put("dessert", "Dessert");
+        menuOptions.put("all", "All");
+
+
+        while (true) {
+
+
+        }
     }
 }
