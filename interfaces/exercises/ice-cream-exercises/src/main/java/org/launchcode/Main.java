6d838c9a1dc6f0d98270e86a81b5bc0267716f91
Alec <alecjost7@gmail.com>
2024-11-07 15:56:51 -0600

    flavor comparator

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -9,7 +9,10 @@ public class Main {
         ArrayList<Cone> cones = menu.getCones();
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        FlavorComparator comparator = new FlavorComparator();
 
+        flavors.sort(comparator);
+        System.out.println(flavors);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
