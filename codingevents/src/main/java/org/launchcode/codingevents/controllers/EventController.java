97f83d459982bd1995bddde26326e368e9ffdf00
Alec <alecjost7@gmail.com>
2024-12-05 20:01:19 -0600

    Hashmap ThymeLeaf

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -10,40 +10,48 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
-@RequestMapping("events")
 public class EventController {
 
 //    private static List<String> events = new ArrayList<>();
-    HashMap<String, String> events = new HashMap<>();
+    final static Map<String, String> events = new HashMap<>();
 
     @GetMapping
+    public String index(){
+        return "home";
+    }
+
+    //lives at /events/create
+    @GetMapping("/create")
+    public String renderCreateEventForm() {
+        return "create";
+    }
+
+    //lives at /events/create
+    @PostMapping("/create")
+    public String createEvent(Model model, @RequestParam String eventName, @RequestParam String eventDescription) {
+//        events.add(eventName);
+        events.put(eventName, eventDescription);
+
+        model.addAttribute("name", eventName);
+        model.addAttribute("description", eventDescription);
+
+        System.out.println(events);
+        return "index";
+    }
+
+    @GetMapping("/events")
     public String displayAllEvents(Model model) {
-       model.addAttribute("event", events);
+        model.addAttribute("allEvents", events);
 
         events.put("Menteaship","A fun meetup for connecting with mentors");
         events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
         events.put("Javascripty", "An imaginary meetup for Javascript developers");
         System.out.println(events);
 
-       return "events/index";
-    }
-
-    //lives at /events/create
-    @GetMapping("create")
-    public String renderCreateEventForm() {
-        return "events/create";
-    }
-
-    //lives at /events/create
-    @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-//        events.add(eventName);
-        events.put(eventName, eventDescription);
-
-        System.out.println(events);
-        return "redirect:/events";
+        return "events";
     }
 }
 
