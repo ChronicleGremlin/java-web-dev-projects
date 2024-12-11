ba1b35fee5f222f6847c3e34a665d0f0b7665462
Alec <alecjost7@gmail.com>
2024-12-11 16:45:47 -0600

    Spa-day Studio

---
+++ Java17-SpaDay-starter/src/main/java/org/launchcode/SpaDayController.java
@@ -0,0 +1,27 @@
+package org.launchcode;
+
+import org.launchcode.controllers.models.Client;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class SpaDayController {
+
+
+    @GetMapping
+    public String customerForm () {
+       return "serviceSelection";
+    }
+
+    @PostMapping()
+    public String spaMenu( Model model, @RequestParam String skintype, @RequestParam String manipedi) {
+
+       Client newClient = new Client(skintype, manipedi);
+       newClient.setAppropriateFacials(skintype);
+       model.addAttribute("client", newClient);
+
+        return "menu";
+    }
+}
